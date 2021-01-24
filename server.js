8c5685d150bded14eeda4af752d00640ea83d481
Anna McIntosh <anlautens@gmail.com>
2021-01-23 21:25:36 -0500

    Updating scripts for deployment

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
 const dotenv = require('dotenv').config();
 const items = require('./routes/api/items.js');
+const path = require("path");
 
 const app = express();
 
@@ -17,6 +18,15 @@ mongoose
     .catch(err => console.log(err));
 
 app.use('/api/items', items);
+
+//Serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 //Set up port
 const port = process.env.PORT || 5000;
