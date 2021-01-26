03d5ba56eeccebf2f8b694054e1415692bfaa6b0
annamcintosh <anlautens@gmail.com>
2021-01-26 17:39:18 -0500

    Creating additional actions on frontend

--- client/src/App.js
+++ client/src/App.js
@@ -6,19 +6,28 @@ import ItemModal from "./components/ItemModal";
 import { Container } from 'reactstrap';
 import { Provider } from 'react-redux';
 import store from './store';
+import { loadUser } from './actions/authActions';
+import { Component } from "react";
 
-function App() {
-  return (
-    <Provider store={store}>
-        <div className="App">
-            <AppNavbar />
-            <Container>
-                <ItemModal />
-                <ShoppingList />
-            </Container>
-        </div>
-    </Provider>
-  );
+class App extends Component {
+
+  componentDidMount() {
+    store.dispatch(loadUser());
+  }
+
+  render() {
+    return (
+      <Provider store={store}>
+          <div className="App">
+              <AppNavbar />
+              <Container>
+                  <ItemModal />
+                  <ShoppingList />
+              </Container>
+          </div>
+      </Provider>
+    );
+  }
 }
 
 export default App;
